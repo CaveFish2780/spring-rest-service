d6d8c251a78386f5fba1745971d437fb0f825c15
Gustav Ljungberg <g.ljungberg@live.se>
2019-03-20 10:44:47 +0100

    modified ProductControler.java, methods complete & cancel

--- src/main/java/org/service/ProductController.java
+++ src/main/java/org/service/ProductController.java
@@ -7,9 +7,17 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.ResourceSupport;
 import org.springframework.hateoas.Resources;
+import org.springframework.hateoas.VndErrors;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -43,4 +51,49 @@ public class ProductController {
 		return new Resources<>(products,
 			linkTo(methodOn(ProductController.class).all()).withSelfRel());
 	}
+	
+	@PostMapping("/products")
+	ResponseEntity<Resource<Product>> newProduct(@RequestBody Product product) {
+		
+		product.setStatus(Status.IN_PROGRESS);
+		Product newProduct = repository.save(product);
+		
+		return ResponseEntity
+				.created(linkTo(methodOn(ProductController.class).one(newProduct.getId())).toUri())
+				.body(assembler.toResource(newProduct));
+	}
+	
+	@PutMapping("/products/{id}/complete")
+	ResponseEntity<ResourceSupport> complete(@PathVariable Long id) {
+		
+	Product product = repository.findById(id).orElseThrow(() -> new ProductNotFoundException(id));
+		
+		if (product.getStatus() == Status.IN_PROGRESS) {
+			product.setStatus(Status.COMPLETED);
+			return ResponseEntity.ok(assembler.toResource(repository.save(product)));
+		}
+	
+		return ResponseEntity
+				.status(HttpStatus.METHOD_NOT_ALLOWED)
+				.body(new VndErrors.VndError("ERROR ","Method not allowed for status: " + product.getStatus()));
+	
+		
+	}
+	
+	@DeleteMapping("/products/{id}/cancel")
+	ResponseEntity<ResourceSupport> cancel(@PathVariable Long id) {
+		
+		Product product = repository.findById(id).orElseThrow(() -> new ProductNotFoundException(id));
+		
+		if (product.getStatus() == Status.IN_PROGRESS) {
+			product.setStatus(Status.CANCELLED);
+			return ResponseEntity.ok(assembler.toResource(repository.save(product)));
+	}
+		
+		return ResponseEntity
+				.status(HttpStatus.METHOD_NOT_ALLOWED)
+				.body(new VndErrors.VndError("ERROR ","Method not allowed for status: " + product.getStatus()));
+	
+	}
+
 }
