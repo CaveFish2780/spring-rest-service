2cbcbb5fba36a7b0c6495abefbfb3538d514b555
Gustav Ljungberg <g.ljungberg@live.se>
2019-03-20 08:31:00 +0100

    modified Product

--- src/main/java/org/service/Product.java
+++ src/main/java/org/service/Product.java
@@ -1,5 +1,30 @@
 package org.service;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
 public class Product {
 
+	private @Id @GeneratedValue Long id;	
+	private String name;
+	private String description;
+	private Status status;
+	
+	public Product() {}
+	
+	
+
+	public Product(String name, String description, Status status) {
+		this.name = name;
+		this.description = description;
+		this.status = status;
+	}
+	
+	
+	
 }
