1b87716225ac7e06e796d38b255529d546663210
Gustav Ljungberg <g.ljungberg@live.se>
2019-03-21 09:31:50 +0100

    modified ProductController

--- src/main/java/org/service/ProductController.java
+++ src/main/java/org/service/ProductController.java
@@ -3,8 +3,11 @@ package org.service;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.ResourceSupport;
@@ -18,6 +21,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -31,6 +35,12 @@ public class ProductController {
 		this.repository=repository;
 		this.assembler=assembler;
 	}
+	
+    @RequestMapping("/")
+    public String helloWorld(HttpServletResponse response)  throws IOException{
+    		  response.getWriter().println("Hello World");
+    		  return "HOME PAGE";
+    }
 	
 	@GetMapping("/products/{id}")
 	Resource<Product> one(@PathVariable Long id) {
